7a53eecc856e98509be660c98d94016b6cf6d4f6
C. Poda <clp302@poda.net>
2016-02-20 19:28:54 -0800

    First commit from learn_python/ dir.
    Rename dir from urlbookmarks to manage_url_bookmarks.
    
    Commit the program file manage_url.py.
    
    Prepare to sync this dir with github.

---
+++ manage_url_bookmarks/manage_url.py
@@ -0,0 +1,320 @@
+#! /usr/bin/python
+
+"""manage_url.py - Manage a list of URL bookmarks w/ name, title, 
+and optional note fields.
+
+TBD: For study group, project 2016_0215.
+"""
+
+"""
+TBD, Add Show 5-10 records on a screen cmd.
+TBD, To support Del cmd:
+    Must know the rec to delete.
+        Use record_id?
+        Specify a current_rec?
+        Top record shown on screen?
+        Find a word; first result is cur rec?
+            If no match, stay at cur rec.
+        Other?
+    go to rec #
+    scroll up & down rec list & change current rec.
+        next and prior record cmds?
+        page up & page down list of records cmds?
+        up & down arrow keys?
+    hilite curr rec in a list?  Or prefix w/ '=>'?
+    delete curr record from dict.
+    Oops, then the record_id strings will have gaps.
+        If I delete rec 2, the list goes from 1 to 3.
+        No big deal.
+    Oops, on sorting by name or URL, keys will be out of order.
+        No big deal.
+"""
+
+"""Plan.
+Read the input file.
+    If i/p file not found in current dir, create it.
+    Assign current record = first record.
+Show current record and menu on Home screen.
+Prompt for user command and wait.
+Interpret user command when entered.
+Perform requested action.
+    Ask user for data for Add, Find, & Update cmds.
+    Ask confirmation for Add, Delete, and Update actions.
+On 'Quit', save data to the file, then quit.
+    TBD, save prior data file as a backup.
+"""
+
+import pickle
+import pprint
+import sys
+
+def PrintHelp():
+    """
+    Show usage and general help data.
+    """
+    usage_text = """\nUsage:
+      manage_url
+        Read data from this file in the current dir: url_bookmarks.pickle.
+        Show the menu and wait for the user's command.
+    """
+
+    menu_help = """Menu commands:
+      A: Add a new record.
+      D: Delete a record; specify ID number; press 'y' to confirm.
+      F: Find; specify a word to find in any record.
+      H: Show help.
+      S: Show all records.
+      Q: Quit the program after saving data to the file.
+      U: Update a record; specify ID number, then enter data at prompt.
+    """
+
+    print usage_text
+    print menu_help
+
+
+def GetFileData(infile):
+    """
+    Open an existing data file and read it.
+    Create a new data file if no data file was found in current dir.
+    """
+    #DEV, Handle initial start when no file exists; then empty file is made.
+    #
+    #DEV, Why does data get erased when program run the second time and quit w/o editing?
+    #
+    num_lines = 0
+    data = {}
+    try:
+        with open(infile, 'rb') as pf:
+            (record_id, data) = pickle.load(pf)
+        print "DBG, gfd, after load, data: ", data
+        if data:  #TBD, rm? Maybe needed when no data in file.
+            for line in data:
+                num_lines += 1
+        return (record_id, data, num_lines)
+    except (EOFError, IOError):
+        print "WARN, file empty or not found in current dir: ", infile
+        print "  Creating an empty data file.  Use Add to fill it with bookmarks."
+        open(infile, 'wb').close()
+    return
+
+
+def GetMenu():
+    menu = "\nPress the first letter of a command (ADFHSQU), then press Enter:\n"
+    menu += "Commands: Add  Delete  Find  Help  Show  Quit  Update\n"
+    # menu += raw_input
+    # user_input = raw_input(menu, "Enter a valid command, "\
+    return menu
+
+
+def AddNewEntry(record_id, url_dict):
+    """
+    Ask user to enter data for a new record.
+    Store it in the data structure.
+    """
+    name = raw_input("Enter the name of the entry> ")
+    url = raw_input("Enter the URL> ")
+    note = raw_input("Optional: Enter a one-line note> ")
+    record_id += 1
+    print "DBG, add, record_id: ", record_id
+    url_dict[record_id] = [name, url, note]
+    return record_id, url_dict
+
+
+def SaveDataToFile(record_id, url_dict):
+    """
+    Convert to binary file w/ pickle & write to disk.
+    """
+    with open('url_bookmarks.pickle', 'wb') as pf:
+        pickle.dump((record_id, url_dict), pf)
+
+
+def GetCommand(menu, record_id, url_dict, infile):
+    """
+    Ask user to enter a character, then check if it is a valid
+    command, and execute it or show an error msg.
+    """
+    #TBRM prompt = menu + "Enter a valid command (ADFHSQU), "\
+            #TBRM + "Ctrl+C to exit without saving > "
+    prompt = menu 
+    while True:
+        try:
+            user_input = raw_input(prompt)
+        except KeyboardInterrupt:
+            print
+            return
+        #DEV (valid, reason) = hw07_1.IsValidIdentifier(user_input)
+        #DEV print "%+20s -> %s" % (user_input, reason)
+        if user_input == 'q' or user_input == 'Q':
+            SaveDataToFile(record_id, url_dict)
+            print "Saving data and quitting program."
+            sys.exit(0)  #TBD, return & quit nicer?
+        elif user_input == 'a' or user_input == 'A':
+            (record_id, url_dict) = AddNewEntry(record_id, url_dict)
+        elif user_input == 'h' or user_input == 'H':
+            PrintHelp()
+        elif user_input == 's' or user_input == 'S':
+            ShowAllRecords(url_dict, infile)
+        elif user_input == 'd' or user_input == 'D':
+            DeleteCurrentRecord(url_dict)
+        elif user_input == 'u' or user_input == 'U':
+            UpdateCurrentRecord(url_dict)
+        elif user_input == 'f' or user_input == 'F':
+            FindWord(url_dict)
+        else:
+            print "Invalid request, not implemented: ", user_input
+    return
+
+
+def ShowBookmarkData(url_dict, book_data, menu):
+    """Show some records starting at the current record.
+    Show some other data also.
+    """
+    pprint.pprint(url_dict)
+    print book_data
+
+
+def DeleteCurrentRecord(url_dict):
+    """Ask user for record to delete.
+    Show the record at the given key.
+    Ask user to confirm delete; & delete it if OK.
+    """
+    key = 0
+    while True:
+        try:
+            key = int(raw_input("Enter the record number to delete: "))
+            current_rec = url_dict[key]
+            break  #TBD, Why is this needed when no except caught?
+        except (KeyError, ValueError, UnboundLocalError):
+            if key:
+                print "\nWARN Problem with this record number: ["+ str(key) + "]" 
+            else:
+                print "\nWARN Problem with this record number."
+            return
+    PrintRecord(url_dict, key)
+    if raw_input("Press 'y' to confirm delete: ") == 'y':
+        try:
+            url_dict.pop(key)
+        except KeyError:
+            "Record number error, try again."
+    else:
+        print "Record was not deleted."
+    #T return current_rec
+
+
+def UpdateCurrentRecord(url_dict):
+    """Ask user for record to update.
+    Show the record at the given key.
+    Ask user to confirm update; & update it if OK.
+    """
+    key = 0
+    while True:
+        try:
+            key = int(raw_input("Enter the record number to update: "))
+            current_rec = url_dict[key]
+            break  #TBD, Why is this needed when no except caught?
+        except (KeyError, ValueError, UnboundLocalError):
+            if key:
+                print "\nWARN Problem with this record number: ["+ str(key) + "]" 
+            else:
+                print "\nWARN Problem with this record number."
+            return
+    PrintRecord(url_dict, key)
+
+    print "\nEnter your updated data: "
+    upd_name = raw_input("  Name: ")
+    upd_url  = raw_input("  URL : ")
+    upd_note = raw_input("  Note: ")
+
+    print "\nUpdated record will be:"
+    print "  Name: ", upd_name
+    print "  URL : ", upd_url
+    print "  Note: ", upd_note
+
+    if raw_input("Press 'y' to confirm update: ") == 'y':
+        try:
+            url_dict[key][0] = upd_name
+            url_dict[key][1] = upd_url
+            url_dict[key][2] = upd_note
+            #O url_dict[key][0] = raw_input("Name: ")
+            #O url_dict[key][1] = raw_input("URL : ")
+            #O url_dict[key][2] = raw_input("Note: ")
+        except KeyError:
+            "Record number error, try again."
+    else:
+        print "Record was not updated."
+    #T return current_rec
+
+
+def ShowAllRecords(url_dict, infile):
+    """Print all records to the screen, w/ key & fields.
+    """
+    bookmark_count = 0
+    for key in url_dict.keys():
+        bookmark_count += 1
+        PrintRecord(url_dict, key)
+    print "Found %d records in data file %s." % (bookmark_count, infile)
+
+
+def PrintRecord(url_dict, key):
+    """Print one record to the screen, w/ key & fields.
+    """
+    print "%2d" % key,
+    print "Name: ", url_dict[key][0]
+    print "   URL : ",  url_dict[key][1]
+    print "   Note: ", url_dict[key][2]
+
+
+def FindWord(url_dict):
+    """Ask user for a word to find in the bookmark list.
+    Search for it and return all records that have it.
+    """
+    bookmark_count = 0
+    num_hits = 0
+    word = raw_input("Enter word to find in bookmark list: ")
+    for key,v in url_dict.items():
+        bookmark_count += 1
+        #D print "DBG, fw, v: ", v
+        #D print
+        for s in v:
+            #D print "DBG, fw, s: ", s
+            #D print
+            if word in s:
+                #D print "DBG, fw, hit, v: ", v
+                #D print
+                num_hits += 1
+                PrintRecord(url_dict, key)
+    print "Found %d hits in %d records." % (num_hits, bookmark_count)
+
+
+def main():
+    """TBD docs for main func.
+    """
+    record_id = 0
+    infile = "url_bookmarks.pickle"
+    url_dict = {}
+    num_lines = 0  #TBD, rm when record_id fixed?
+
+    try:
+        (record_id, url_dict, num_lines) = GetFileData(infile)
+    except TypeError:
+        print "WARN: No data in file?  Add bookmark records to the file to continue."
+    #TBRM record_id = num_lines  # Next record will get correct ID.
+    #TMP bookmark_sum_data = "Number of records: ", num_lines
+    menu = GetMenu()
+    #TMP ShowBookmarkData(url_dict, bookmark_sum_data, menu)
+        # Show summary data, eg, number of records in file.
+        # Show first record and make it the current record.
+        # Show menu summary.
+    GetCommand(menu, record_id, url_dict, infile)
+        # AddNewEntry()
+        # DeleteEntry()
+        # PrintHelp()
+        # Quit
+            # SaveDataToFile()
+        # FindWord()
+        # UpdateEntry()
+
+if __name__=='__main__':
+    main()
+
+
