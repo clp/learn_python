6c396c4535f697f055651a7f0fd26bbf7355808f
C. Poda <clp302@poda.net>
2017-04-18 20:03:53 -0700

    Change many empty lines to hold a TAB char.
    This change allows me to view the file's structure rather easily.
    
    The file appears to have empty lines when viewed in normal mode w/ a
    text editor.  When viewed as a VimOutliner file, the content of the file
    can be collapsed and expanded easily based on indentation, to show its
    structure.  This can be a useful feature for understanding the code and
    for debugging.

--- find_answers/nltk_ex25.py
+++ find_answers/nltk_ex25.py
@@ -1,38 +1,38 @@
 # -*- coding: utf-8 -*-
 # Using ~/anaconda3/bin/python: Python 3.6.0 :: Anaconda 4.3.0 (64-bit)
 
-# Time-stamp: <Fri 2017 Apr 14 04:20:41 PMPM clpoda>
+# Time-stamp: <Tue 2017 Apr 18 08:00:26 PMPM clpoda>
 
 """nltk_ex25.py
-
+    
    Analyze text files from stackoverflow.com data using NLTK.
-
+    
    NLTK is the Natural Language Toolkit, a suite of 
    Python s/w, data sets, and documents to support
    the development of Natural Language Processing.
-
+    
    The input file has one question and multiple answers for that
    question.
-
+    
    A goal is to find answers in stackoverflow that might be
    good, but 'hidden' because they have low scores.
    Look for such answers based on the terms found in high-score
    answers, that are also found in low-score answers.
    Save the o/p to a file for further evaluation & processing.
-
+    
    Usage:
      pydoc  nltk_ex25
      python nltk_ex25.py
-
+    
      Set the value of num_hi_score_terms at the bottom of the file;
      It determines how many hi-score terms are used to search
      text in low-score answers.  A bigger number will cause the
      program to run longer, and might find more 'hidden' answers
      that could be valuable.
-
+    
    Options:
      TBD
-
+    
 ------
 """
 
@@ -66,9 +66,6 @@ logger = logging.getLogger(__name__)
 log_msg = log_file + " - Start logging.\n"
 logger.info(log_msg)
 
-
-
-
 a_infile  = ""
 
 def main():
@@ -78,27 +75,27 @@ def main():
             progress_msg_factor, numlines, clean_ans_bodies_l, \
             vocab, dist, words_sorted_by_count_main_l, df_score, \
             num_selected_recs, top, df_score_top_n 
-    #
+    
     a_fname, a_infile, q_infile, datadir, tmpdir, outdir = config_data()
-    #
+    
     logger.info("Step 1. Read data from file into dataframes.")
     all_ans_df, all_ques_df, progress_msg_factor, numlines = read_data(a_infile, q_infile)
-    #
+    
     logger.info("Step 2. Process the words of each input line.")
     clean_ans_bodies_l = clean_raw_data()
-    #
+    
     logger.info("Step 3. Build a bag of words and their counts.")
     (vocab, dist) = make_bag_of_words(clean_ans_bodies_l)
     words_sorted_by_count_l = sort_save_vocab('.vocab')
     # Save the original list for later searching.
     words_sorted_by_count_main_l = words_sorted_by_count_l
-    #
+    
     logger.info('Step 4. Sort Answers by Score.')
     df_score, num_selected_recs = sort_answers_by_score()
-    #
+    
     logger.info('Step 5. Find most freq words for top-scoring Answers.')
     df_score_top_n = df_score[['Id']]
-    #
+    
     #OK.tbr  print("DBG.103", df_score_top_n.tail(num_selected_recs), '\n')
     #TBD, Maybe convert df to string so logger can print title & data w/ one cmd:
     #TBD log_msg = "df_score_top_n.tail():" + CONVERT_DF_TO_STRING(df_score_top_n.tail())
@@ -107,13 +104,13 @@ def main():
     logger.debug(df_score_top_n.tail(20)) 
     # Use top_n Answers & count their words.
     logger.info("For top ans: Cleaning and parsing the training set bodies...")
-    #
+    
     top = True
     top_n_bodies = find_freq_words()
     logger.info('make_bag_of_words(top_n_bodies)')
     (vocab, dist) = make_bag_of_words(top_n_bodies)
     sort_save_vocab('.vocab.hiscore')
-    #
+    
     logger.info("Step 6. Find most frequent words for bottom-scoring Answers.")
     # Keep these data to compare w/ words for top-scoring Answers; s/b some diff.
     # If they are identical, there may be a logic problem in the code.
@@ -126,22 +123,22 @@ def main():
     logger.info('make_bag_of_words(bot_n_bodies)')
     (vocab, dist) = make_bag_of_words(bot_n_bodies)
     sort_save_vocab('.vocab.loscore')
-    #
+    
     logger.info("Step 7. Search lo-score A's for hi-score text.")
     search_for_terms()
-
-
+    
+    
 def init():
     """Initialize some settings for the program.
     """
     # Initialize settings for pandas.
     pd.set_option('display.width', 0)  # 0=no limit, use for debugging; try 120.
-    #
+    
     # Don't show commas in large numbers.
     # Show OwnerUserId w/o '.0' suffix.
     pd.options.display.float_format = '{:.0f}'.format
-
-
+    
+    
 def config_data():
     """Configure path and file names for i/o data.
     """
@@ -150,7 +147,7 @@ def config_data():
     outdir = 'outdir/'  # Relative to current dir
     #D q_fname = 'Questions.csv'
     #D a_fname = 'Answers.csv'
-    #
+    
     # Smaller data sets, used for debugging.
     #D a_fname = 'a6_999999.csv'  
     #D a_fname = 'a5_99998.csv'  # Bag has 7903 rows.
@@ -162,22 +159,22 @@ def config_data():
     #D a_fname = 'q_with_a.40_owners_a5_9998.csv'  # 800 lines; O/p from fga*.py
     q_fname = 'q2.csv'
     #D a_fname = 'a2.csv'
-    #
+    
     # Choose tmpdir or datadir:
     #D a_infile = tmpdir  + a_fname
     #D a_infile = datadir + a_fname
     a_infile = outdir + a_fname
-    #
+    
     # Choose tmpdir or datadir:
     #D q_infile = tmpdir  + q_fname
     q_infile = datadir + q_fname
-    #
+    
     print('\nInput files, q & a:\n'  + q_infile + '\n' + a_infile)
     logger.info('Input files, q & a:\n'  + q_infile + '\n' + a_infile)
-    #
+    
     return a_fname, a_infile, q_infile, datadir, tmpdir, outdir
-
-
+    
+    
 def read_data(ans_file, ques_file):
     """Read the csv i/p files and store into a pandas data frame.
     Compute a factor that dictates how progress will be indicated
@@ -187,7 +184,7 @@ def read_data(ans_file, ques_file):
     #TBD question file is not used now. Use empty df for ques_df now.
     #D ques_df = pd.read_csv(ques_file, encoding='latin-1', warn_bad_lines=False, error_bad_lines=False)
     ques_df = pd.DataFrame()
-
+    
     numlines = len(ans_df)
     print('\nNumber of answer records in i/p data frame, ans_df: ' + str(numlines))
     logger.info('Number of answer records in i/p data frame, ans_df: ' + str(numlines))
@@ -220,38 +217,38 @@ def convert_text_to_words( raw_q_a ):
     """
     # 1. Remove HTML
     q_a_text = BeautifulSoup(raw_q_a, "lxml").get_text()
-    #
+    
     # 2. Remove non-letters
     letters_only = re.sub("[^a-zA-Z]", " ", q_a_text)
-    #
+    
     # 3. Convert to lower case, split into individual words
     words = letters_only.lower().split()
-    #
+    
     # 4. In Python, searching a set is much faster than searching
     #   a list, so convert the stop words to a set
     stops = set(stopwords.words("english"))
-    #
+    
     # Add more noise terms to stopwords.
     stops.add('th')
-    #
+    
     # 5. Remove stop words
     meaningful_words = [w for w in words if not w in stops]
-    #
+    
     # 6. Join the words back into one string separated by space,
     # and return the result.
     return( " ".join( meaningful_words ))
-
-
+    
+    
 def clean_raw_data():
     """ For all answers: Clean and parse the training set bodies.")
     """
     # Get the number of bodies based on that column's size
     num_bodies = all_ans_df["Body"].size
     logger.info("Number of bodies: " + str(num_bodies))
-    #
+    
     clean_ans_bodies_l = []
     all_ans_df["CleanBody"] = ""
-    #
+    
     # Build a list that holds the cleaned text from each answer's body field.
     # Use it to find terms that match terms found in hi-score Answers.
     for i in range( 0, num_bodies ):
@@ -266,7 +263,7 @@ def clean_raw_data():
             #D logger.debug("Body %d of %d" % ( i+1, num_bodies ))
             #D logger.debug('  Original text: ' + all_ans_df['Body'][i])
             #D logger.debug('  Cleaned text:  ' + clean_q_a)
-    #
+    
     # Write cleaned bodies to a file, one body per line, for visual review.
     outfile = tmpdir + a_fname + '.out'
     if os.path.exists(outfile):
@@ -274,25 +271,25 @@ def clean_raw_data():
     with open(outfile, 'w') as f:
         f.write('\n'.join(clean_ans_bodies_l))
     return clean_ans_bodies_l
-
-
+    
+    
 def make_bag_of_words(clean_ans_bodies_l):
     """Collect and count words (or phrases = ngrams) in the text.
-
+    
     To use ngrams instead of single words in the analysis,
     specify 'ngram_range = (3,5)' (for example) in the args for
     CountVectorizer.
-
+    
     This arg includes 1-letter words: 'token_pattern = r'\b\w+\b'.
     """
     logger.debug("Creating the bag of words for word counts ...")
     from sklearn.feature_extraction.text import CountVectorizer
-
+    
     # Initialize the "CountVectorizer" object, which is scikit-learn's
     # bag of words tool
-    #
+    
     # TBD, Fails w/ MemErr with max_features at 5000; ok at 100-200.
-    #
+    
     vectorizer = CountVectorizer(analyzer = "word",   \
                                  tokenizer = None,    \
                                  preprocessor = None, \
@@ -300,39 +297,38 @@ def make_bag_of_words(clean_ans_bodies_l):
                                  ngram_range = (3,5), \
                                  # token_pattern = r'\b\w+\b', \
                                  max_features = 200)
-
+    
     # fit_transform() does two functions: First, it fits the model
     # and learns the vocabulary; second, it transforms our training data
     # into feature vectors. The input to fit_transform should be a list of
     # strings.
-    #
+    
     train_data_features = vectorizer.fit_transform(clean_ans_bodies_l)
-
+    
     # Numpy arrays are easy to work with, so convert the result to an
     # array
-    #
+    
     train_data_features = train_data_features.toarray()
-
+    
     # Note, w/ q9999 data, got (727, 1550):
     logger.info('Bag shape: rows (num of records), cols (num of features):')
     logger.info(train_data_features.shape)
-
+    
     # Take a look at the words in the vocabulary
     vocab = vectorizer.get_feature_names()
-
+    
     # Print counts of each word in vocab.
     # Sum up the counts of each vocabulary word
     dist = np.sum(train_data_features, axis=0)
-
+    
     return (vocab, dist)
-
-
-
+    
+    
 def sort_save_vocab(suffix):
     """
     Sort and save vocabulary data to a list and to a file
     with a specified suffix.
-
+    
     For each item in the bag of words, print the vocabulary word and
     the number of times it appears in the training set
     """
@@ -342,10 +338,10 @@ def sort_save_vocab(suffix):
     for tag, count in zip(vocab, dist):
         count_tag_l.append((count, tag))
         #D word_freq_d[tag] = count
-    #
+    
     # Sort the list of tuples by count.
     words_sorted_by_count_l = sorted(count_tag_l, key=lambda x: x[0])
-    #
+    
     # Write sorted vocab to a file.
     outfile = tmpdir + a_fname + suffix
     if os.path.exists(outfile):
@@ -354,15 +350,15 @@ def sort_save_vocab(suffix):
         for count, word in words_sorted_by_count_l:
             print(count, word, file=f)
     return words_sorted_by_count_l 
-
-
+    
+    
 def sort_answers_by_score():
     """
     Build a sorted dataframe of answers.
     """
     df_score = all_ans_df.sort_values(['Score'])
     df_score = df_score[['Id', 'Score']]
-    #
+    
     # Compute the number of records to use for computation and display.
     rec_selection_ratio = 0.10  # Default 0.01?
     num_selected_recs = int(numlines * rec_selection_ratio)
@@ -376,10 +372,10 @@ def sort_answers_by_score():
     logger.info('Highest scoring Answers:')
     logger.info(df_score.tail())
     #D print(df_score.tail(num_selected_recs), '\n')
-    #
+    
     return df_score, num_selected_recs
-
-
+    
+    
 def find_freq_words():
     """
     Build a list of the most frequent terms found in answers.
@@ -403,34 +399,34 @@ def find_freq_words():
             #D logger.debug('  Original text:\n' + df8['Body'][i][:70])
             #D logger.debug('  Cleaned text:\n' + clean_q_a[:70])
     return top_n_bodies 
-
-
+    
+    
 def search_for_terms():
     """
     Read each answer and save any terms that it has in common
     with (high frequency) text from the high-score answers.
-
+    
     Save those answers for further investigation.
     """
-    #
+    
     clean_ans_bodies_df = pd.DataFrame(clean_ans_bodies_l)
     logger.debug("clean_ans_bodies_l, top:") 
     logger.debug(clean_ans_bodies_l[:1]) 
     logger.debug("clean_ans_bodies_l, bottom:")
     logger.debug(clean_ans_bodies_l[-1:]) 
-    #
+    
     #D Use short list cab_l for testing:
     #D cab_l = ['step isprimenumber call', 'foo step isprimenumber call bar', 'yield abc def ghi generators long first string', 'generator', 'foo next function bar', 'short list foo bar baz']
-    #
+    
     #D cab_df = pd.DataFrame(cab_l)
-    #
+    
     #TBF
     # p.1. Separate the terms from each other so it is clear what was found
     #  and what was not found in the A's being checked.
     #  Should tmp_df not be a df?  Would a list of lists be better?
-    #
+    
     all_ans_df["HiScoreTerms"] = ""
-    #
+    
     print("\nTerms from hi-score Answers.")
     logger.info("Terms from hi-score Answers.")
     #TBD, Maybe collect these "count,w" data into a single list,
@@ -445,15 +441,15 @@ def search_for_terms():
                 #TBD, Change string 'w' to list or tuple for better storage in df?
                 #OK all_ans_df.loc[index, "HiScoreTerms"] = all_ans_df.loc[index, "HiScoreTerms"] + w + ' , '
                 all_ans_df.loc[index, "HiScoreTerms"] += ( w + ' , ')
-    #
+    
     #D print()
     #D logger.debug("DBG, all_ans_df.head():")
     #D logger.debug(all_ans_df.head())
-    #
+    
     # Save full df to a file.
     outfile = "tmpdir/all_ans.csv"
     all_ans_df.to_csv(outfile)
-    #
+    
     # Save possible valuable answers to a separate file for review.
     # Replace empty strings in HiScoreTerms cells with NaN, for easier manipulation.
     all_ans_df['HiScoreTerms'].replace('', np.nan, inplace=True)
@@ -461,12 +457,12 @@ def search_for_terms():
     ans_with_hst_df = all_ans_df.dropna(subset=['HiScoreTerms'])
     outfile = "tmpdir/ans_with_hst.csv"
     ans_with_hst_df.to_csv(outfile)
-    #
+    
     # Print partial data about interesting answers to check.
     print("\nCheck low score Answers for useful data: ")
     print(ans_with_hst_df[['Id', 'Score', 'CreationDate', 'Title']])
     print(ans_with_hst_df[['Id', 'HiScoreTerms']])
-    #
+    
     # Also write summary data to log.
     logger.info("Check low score Answers for useful data: ")
     logger.info(ans_with_hst_df[['Id', 'Score', 'CreationDate', 'Title']])
