40f94bdbbb9070f3dbacf42d6cd8e23292c89fea
C. Poda <clp302@poda.net>
2018-01-16 13:10:09 -0800

    fga: Move 3 menu & UI funcs from fga to a new module, text_ui.py.
    Move show_menu(), build_stats(), and check_owner_reputation() into
    the new module.
    
    Declutter fga by moving out discrete functionality that is not a core
    part of the project.
    
    The new module will be added to the repo in the next commit.

--- find_answers/fga_find_good_answers.py
+++ find_answers/fga_find_good_answers.py
@@ -14,7 +14,7 @@ based on their other questions & answers.
 Save the o/p to a file for further evaluation & processing.
 
 Usage:
-    fga_find_good_answers.sh
+    fga_find_good_answers.sh [-h]
     python fga_find_good_answers.py
     pydoc  fga_find_good_answers
 
@@ -54,16 +54,7 @@ Overview of Program Actions
     histogram plots.  Several menu choices require the NLP-ready
     data.
 
-    Some menu actions use the following functions.
-
-    build_stats():
-    Compute and save statistics about the selected data, to use
-    in further analysis and plotting, eg, owner reputation (mean
-    score) and length of the body text of the record.
-
-    check_owner_reputation():
-    Provide reputation data from a dataframe or a file.  If it
-    does not exist, call the function to build it.
+    Some actions use the following functions.
 
     group_data():
     Group the data by owner, and sort by mean score.
@@ -83,6 +74,7 @@ Other Actions
     records that contain it, and to save them to a file.
 
 ----------------------------------------------------------
+
 
 Input data format of stackoverflow.com python file from kaggle.com.
 
@@ -144,6 +136,7 @@ import config as cf
 import draw_plots as dr
 import nltk_ex25 as nl
 import sga_show_good_answers as sga
+import text_ui as tui
 import util.write as wr
 
 
@@ -155,8 +148,6 @@ cf.logger.info(
 
 DATADIR = cf.DATADIR
 FILEA3 = 'a3_986.csv'
-FOOTER = cf.FOOTER
-HEADER = cf.HEADER
 INDIR = 'indir/'
 MAX_COL_WID = cf.MAX_COL_WID
 MAX_HI_SCORE_TERMS = 100 # 10
@@ -207,7 +198,6 @@ def main(popular_qa_df):
         combine_related_q_and_a(
             ques_ids_pop_and_top_l, all_ques_df, all_ans_df)
 
-    #ORG columns_l = ['Id', 'ParentId', 'Title', 'Body', 'HSTCount', 'Score']
     columns_l = ['HSTCount', 'Score', 'Id', 'ParentId', 'Title', 'Body']
     if keyword:
         qa_with_keyword_df = sga.select_keyword_recs(
@@ -592,7 +582,6 @@ def analyze_text(qagroup_from_pop_top_ques_df):
         print(
             'analyze_text(): Questions in qagroup_from_pop_top_ques_df: Id, Score, Title:')
         print(
-            #ORG qagroup_from_pop_top_ques_df)
             qagroup_from_pop_top_ques_df[['Id', 'Score', 'Title']].iloc[0].values)
     clean_ans_bodies_l = nl.clean_raw_data(qagroup_from_pop_top_ques_df)
     if not clean_ans_bodies_l:
@@ -612,254 +601,6 @@ def analyze_text(qagroup_from_pop_top_ques_df):
         [popular_qa_df, qa_with_hst_df]).reset_index(drop=True)
 
     return popular_qa_df
-
-
-def show_menu(qa_df, all_ans_df, owner_reputation_df, opt_ns):
-    """Show prompt to user; get and handle their request.
-    """
-    user_menu = """    The menu choices:
-    drm: calculate reputation matrix of owners
-    dsm: draw and plot q&a statistics matrix
-    d: draw default plot of current data
-    dh: draw default histogram plot of current data
-    dm: draw scatter matrix plot of current data
-    h, ?: show help text, the menu
-    lek: look for exact keywords in questions and answers
-    m: show menu
-    q: save data and quit the program
-    s: show current item: question or answer
-    sn: show next item: question or answer
-    sp: show prior item: question or answer
-    """
-    user_cmd = ''
-    saved_index = 0
-
-    # Show prompt & wait for a cmd.
-    print("======================\n")
-    cmd_prompt = "Enter a command: q[uit] [m]enu  ...  [h]elp: "
-    while user_cmd == "":  # Repeat the prompt if no i/p given.
-        user_cmd = input(cmd_prompt)
-    print("User entered this cmd: ", user_cmd)
-
-    # Loop to handle user request.
-    while user_cmd:
-        if user_cmd.lower() == 'm':
-            print(user_menu)
-            # Clear user_cmd here to avoid infinite repetition of while loop.
-            user_cmd = ''
-        elif user_cmd.lower() == 'q':
-            print("Quit the program.")
-            log_msg = cf.log_file + ' - Quit, user req; Finish logging for ' + \
-                os.path.basename(__file__) + '\n'
-            cf.logger.warning(log_msg)
-            raise SystemExit()
-        elif user_cmd == '?' or user_cmd == 'h':
-            print(user_menu)
-            user_cmd = ''
-        elif user_cmd.lower() == 's':
-            user_cmd = ''
-            if qa_df.empty:
-                print("WARN: dataframe empty or not found; try restarting.")
-            else:
-                print(qa_df[['Id', 'Title', 'Body']].iloc[[saved_index]])
-        elif user_cmd.lower() == 'sn':  # show next item
-            user_cmd = ''
-            if qa_df.empty:
-                print("WARN: dataframe empty or not found; try restarting.")
-            else:
-                saved_index += 1
-                print(qa_df[['Id', 'Title', 'Body']].iloc[[saved_index]])
-        elif user_cmd.lower() == 'sp':  # show prior item
-            user_cmd = ''
-            if qa_df.empty:
-                print("WARN: dataframe empty or not found; try restarting.")
-            else:
-                saved_index -= 1
-                print(qa_df[['Id', 'Title', 'Body']].iloc[[saved_index]])
-        elif user_cmd.lower() == 'd':
-            user_cmd = ''
-            if popular_qa_df.empty:
-                print("WARN: dataframe empty or not found; try restarting.")
-            else:
-                print("Drawing the default plot, with Score and HSTCount.")
-                dr.draw_scatter_plot(
-                    popular_qa_df,
-                    'Score',
-                    'HSTCount',
-                    'Score',
-                    'HSTCount')
-        elif user_cmd.lower() == 'dh':
-            user_cmd = ''
-            if popular_qa_df.empty:
-                print("WARN: dataframe empty or not found; try restarting.")
-            else:
-                print("Drawing the default histogram plot.")
-                dr.draw_histogram_plot(popular_qa_df)
-        elif user_cmd.lower() == 'dm':  # Scatter matrix plot
-            user_cmd = ''
-            if popular_qa_df.empty:
-                print("WARN: dataframe empty or not found; try restarting.")
-            else:
-                print("Drawing the default scatter matrix plot.")
-                dr.draw_scatter_matrix_plot(popular_qa_df)
-        # drm: Draw Reputation matrix, mean, answers only; scatter.
-        elif user_cmd.lower() == 'drm':
-            user_cmd = ''
-            owner_reputation_df = check_owner_reputation(
-                all_ans_df, owner_reputation_df)
-            #
-            if owner_reputation_df.empty:
-                print("WARN: owner reputation dataframe empty or not found.")
-            else:
-                print("NOTE: Drawing the owner reputation scatter matrix plot.")
-                dr.draw_scatter_matrix_plot(
-                    owner_reputation_df[['MeanScore', 'OwnerUserId']])
-        # dsm: Draw q&a statistics matrix
-        elif user_cmd.lower() == 'dsm':
-            user_cmd = ''
-            owner_reputation_df = check_owner_reputation(
-                all_ans_df, owner_reputation_df)
-            #
-            qa_stats_df = build_stats(popular_qa_df, owner_reputation_df)
-            #
-            if qa_stats_df.empty:
-                print("WARN: qa_stats_df empty or not found.")
-            else:
-                print("NOTE: Drawing the qa_stats_df scatter matrix plot.")
-                dr.draw_scatter_matrix_plot(
-                    qa_stats_df[['Score', 'BodyLength', 'OwnerRep', 'HSTCount']])
-        # lek: Look for exact keywords in the Q&A df; now case sensitive.
-        elif user_cmd.lower() == 'lek':
-            user_cmd = 'lek'  # Force menu to always repeat the lek prompt.
-            search_prompt = "\nType a search term; or press Enter for main menu: "
-            search_term = input(search_prompt)
-            if search_term == "":  # Return to main menu and ask for a cmd.
-                user_cmd = 'm'
-                continue
-            print("User entered this search_term: ", search_term)
-            log_msg = cf.log_file + ' - Search term entered by user: ' + \
-                search_term + '\n'
-            cf.logger.warning(log_msg)
-            #
-            columns_l = [
-                'HSTCount',
-                'Score',
-                'Id',
-                'ParentId',
-                'Title',
-                'Body',
-                'HiScoreTerms']
-            q_a_group_with_keyword_df = pd.DataFrame()  # Initlz at each call
-            #
-            # TBD Chg popular_qa_df to a df w/ more records, for dbg & initial
-            # use.  Beware of memory & performance issues.
-            q_a_group_with_keyword_df = sga.select_keyword_recs(
-                search_term, popular_qa_df, columns_l, opt_ns)
-        else:
-            print("Got bad cmd from user: ", user_cmd)
-            print(user_menu)
-            # Clear user_cmd here to avoid infinite repetition of while loop.
-            user_cmd = ''
-        # Show prompt & wait for a cmd.
-        print("======================\n")
-        while user_cmd == "":  # Repeat the prompt if no i/p given.
-            user_cmd = input(cmd_prompt)
-    print("#D End of the cmd interpretation loop; return.\n")
-
-    return
-
-
-def build_stats(qa_df, or_df):
-    """Build a table of statistical data about the data, for
-    analysis and plotting.
-
-    Plan:
-    Loop on all records in qa_df:
-        Read record from qa_df.
-        Find OUId.
-        Read Owner Reputation df.
-            Find that OUId.
-            Write Owner Rep to qa_stats_df in its column.
-    Print table.
-    Plot data in scatter matrix.
-    Visually look for records with high Reputation and low Score.
-    """
-    qa_stats_df = qa_df[['Id', 'OwnerUserId', 'ParentId', 'Score', 'HSTCount']]
-    # Add new column to df & initlz it.
-    qa_stats_df = qa_stats_df.assign(BodyLength=qa_stats_df.Id)
-
-    for index, row in qa_df.iterrows():
-        ouid = row['OwnerUserId']
-        try:
-            owner_rep = round(
-                or_df.loc[or_df['OwnerUserId'] == ouid, 'MeanScore'].iloc[0])
-            # Add new column to df.
-            qa_stats_df.loc[index, 'OwnerRep'] = owner_rep
-        except IndexError:
-            # TBD, Some answers in the data file were made by Owners
-            # who are not yet in the reputation df.
-            print(
-                "build_stats(): ouid not in owner reputation dataframe;",
-                " index,ouid: ",
-                index,
-                ouid)
-            print("build_stats():data from the problem row in qa_df:\n", row)
-            print()
-
-        # Save length of body text of each answer.
-        qa_stats_df.loc[index, 'BodyLength'] = len(row['Body'])
-
-    qa_stats_df = qa_stats_df[['Id',
-                               'ParentId',
-                               'OwnerUserId',
-                               'Score',
-                               'BodyLength',
-                               'OwnerRep',
-                               'HSTCount']]
-
-    stats_fname = DATADIR + 'qa_stats_by_dsm.csv'
-    wr.save_prior_file('', stats_fname)
-    qa_stats_df.to_csv(stats_fname)
-
-    stats_fname = DATADIR + 'qa_stats_by_dsm.html'
-    wr.save_prior_file('', stats_fname)
-    qa_stats_df.to_html(stats_fname)
-
-    return qa_stats_df
-
-
-def check_owner_reputation(all_ans_df, owner_reputation_df):
-    """Check for dataframe with reputation of each OwnerUserId.
-    If not found, then calculate reputation of each OwnerUserId
-    in the i/p data, based on Score of all answers they provided.
-    Save the data to a disk file and use it when needed, so the
-    calculation need not be done every time this program runs.
-    """
-    own_rep_file = DATADIR + 'owner_reputation.csv'
-    # TBD Must chk i/p file & replace owner_rep*.csv if
-    #  a different file was used.  OR, just build this file from Answers.csv
-    #  which should have all answers & produce good reputation data.
-
-    if not owner_reputation_df.empty:
-        return owner_reputation_df
-
-    if os.path.exists(own_rep_file):
-        print("owner rep file, " + own_rep_file + ", found; read it.")
-        owner_reputation_df = pd.read_csv(
-            own_rep_file,
-            encoding='latin-1',
-            warn_bad_lines=False,
-            error_bad_lines=False)
-        return owner_reputation_df
-    else:
-        print(
-            "owner rep file, " +
-            own_rep_file +
-            ", not found; build it.")
-        print("This should be a one-time operation w/ data saved on disk.")
-        owner_reputation_df = gd2_group_data(all_ans_df)
-        owner_reputation_df.to_csv(own_rep_file)
-    return owner_reputation_df
 
 
 def get_parser():
@@ -923,9 +664,7 @@ if __name__ == '__main__':
         cf.logger.warning(log_msg)
         raise SystemExit()
 
-    owner_reputation_df = pd.DataFrame()
-
-    show_menu(popular_qa_df, all_ans_df, owner_reputation_df, opt_ns)
+    tui.show_menu(popular_qa_df, all_ans_df, opt_ns)
 
     log_msg = cf.log_file + ' - Finish logging for ' + \
         os.path.basename(__file__) + '\n'
