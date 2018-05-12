5329de17d9ffc4b8fe85176afbc5258d28cd5b14
C. Poda <clp302@poda.net>
2018-05-11 17:03:12 -0700

    sga_show_good_answers: Specify list of columns to use in two places.
    One clause requires all columns of the dataframe be used.
    Change the name from columns_l to columns_all_l.
    
    Use the list sent by the caller; or use a default
    list if none was sent.

--- find_answers/sga_show_good_answers.py
+++ find_answers/sga_show_good_answers.py
@@ -171,10 +171,13 @@ def select_keyword_recs(keyword, qa_df, columns_l, opt_ns):
     qa_contains_sr = qab_sr | qt_sr
     #
     # Build a df with Q's and A's that contain the keyword.
+    if not columns_l:
+        columns_l = ['HSTCount', 'Score', 'Id', 'ParentId', 'Title', 'Body']
     qak_df = qa_df[columns_l][qa_contains_sr]
     if qak_df.empty:
         print('sga.select*():qak: Search term not found, qak_df is empty.')
         # Return empty df, so caller does not fail.
+        #TBD, return qak_df?
         return pd.DataFrame()
     #
     # Print summary, 1 line/record:
@@ -312,13 +315,14 @@ def select_keyword_recs(keyword, qa_df, columns_l, opt_ns):
     #
     search_fname = 'search_result_all_fields.html'
     wr.save_prior_file(DATADIR, search_fname)
-    columns_l = ['HSTCount', 'Score', 'Id', 'ParentId', 'Title', 'Body', 'CreationDate', 'OwnerUserId', 'CleanBody', 'HiScoreTerms']
-    wr.write_df_to_html(qa_keyword_df, DATADIR, search_fname, columns_l)
+    columns_all_l = ['HSTCount', 'Score', 'Id', 'ParentId', 'Title', 'Body', 'CreationDate', 'OwnerUserId', 'CleanBody', 'HiScoreTerms']
+    wr.write_df_to_html(qa_keyword_df, DATADIR, search_fname, columns_all_l)
     print('NOTE: See search output in this file: ' + DATADIR + search_fname)
     #
     search_fname = 'search_result_full.html'
     wr.save_prior_file(DATADIR, search_fname)
-    columns_l = ['HSTCount', 'Score', 'Id', 'ParentId', 'Title', 'Body']
+    #TBR if not columns_l:
+        #TBR columns_l = ['HSTCount', 'Score', 'Id', 'ParentId', 'Title', 'Body']
     wr.write_df_to_html(qa_keyword_df, DATADIR, search_fname, columns_l)
     print('NOTE: See search output in this file: ' + DATADIR + search_fname)
     #
